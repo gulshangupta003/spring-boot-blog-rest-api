941bed149292040e958498c5398242d8f8e07c52
Gulshan <gulshan@Gulshans-MacBook-Pro.local>
2024-08-23 13:06:45 +0530

    Created updateCategory REST API

--- src/main/java/com/springboot/blog/service/CategoryService.java
+++ src/main/java/com/springboot/blog/service/CategoryService.java
@@ -10,4 +10,6 @@ public interface CategoryService {
     CategoryDto getCategory(long categoryId);
 
     List<CategoryDto> getAllCategories();
+
+    CategoryDto updateCategory(CategoryDto categoryDto, long categoryId);
 }
